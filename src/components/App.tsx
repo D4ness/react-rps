7816b79e825bf340b2f2ff74f9b719404bda05a9
D4ness <lyakhov016@gmail.com>
2024-03-29 23:52:39 +0300

    Rework Footer and Header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.scss';
-import {Header} from "@/components/Header/Header";
 import {Routing} from "@/components/Routing/Routing";
-
-
 
 function App () {
     return (
         <div className="App">
-            <Header/>
             <Routing />
         </div>
     )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,14 @@ import logo from '@/assets/images/logo.svg'
 import './header.scss';
 import {Counter} from "@/components/Counter/Counter";
 
-export const Header = () => {
+interface IProps {
+    counterValue: number;
+}
+export const Header = ({counterValue}: IProps) => {
     return (
         <header className='header'>
             <img src={logo} alt='logo' className='header__logo' />
-            <Counter />
+            <Counter counterValue={counterValue} />
         </header>
     );
 };
