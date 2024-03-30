96834ae3dcdbee9ed9f8f2305a0b13fc1b5427d1
D4ness <lyakhov016@gmail.com>
2024-03-30 12:49:50 +0300

    gh pages settings

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@ import App from './components/App'
 import {BrowserRouter} from 'react-router-dom'
 import '@/assets/globals.scss';
 
+const basename = process.env.NODE_ENV === 'production' ? '/react-rps' : '/';
 
 const container: HTMLElement = document.getElementById("root")
 createRoot(container).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={basename}>
             <Suspense fallback="Loading...">
                 <App/>
             </Suspense>
